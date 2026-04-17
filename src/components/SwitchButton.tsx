d6ec0fa65341a4f53994398d8d45b002323ffb5c
Didusantos <diduporto@hotmail.com>
2026-04-16 22:10:17 -0300

    Added: Switch Button component

---
+++ src/components/SwitchButton.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { StyleSheet, Text, TextProps, View } from "react-native";
+import { Switch } from "react-native-paper";
+
+type CustomTextProps = TextProps & {
+  label: string;
+};
+
+export function SwitchButton({ label, ...rest }: CustomTextProps) {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  return (
+    <View style={styles.container}>
+      <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      <Text>{label}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#000",
+  },
+});
