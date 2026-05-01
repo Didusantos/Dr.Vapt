6ff5e9a33f4b912466e1cc5b59da05ed54f03564
Didusantos <diduporto@hotmail.com>
2026-04-30 22:08:40 -0300

    Added: Family View

---
+++ src/app/(tabs)/family.tsx
@@ -0,0 +1,35 @@
+import {
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
+
+export default function HistoricScreen() {
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.select({ ios: "padding", android: "height" })}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.container}>
+          <Text>Tela dos familiares</Text>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 32,
+  },
+});
